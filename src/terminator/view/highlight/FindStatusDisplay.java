a765e90881275402de0053921ecdd0aff82608f3
Nikolai Weibull <now@bitwi.se>
2009-09-03 22:14:06 +0200

    Remove unused FindStatusDisplay class

--- src/terminator/view/highlight/FindStatusDisplay.java
+++
@@ -1,8 +0,0 @@
-package terminator.view.highlight;
-
-public interface FindStatusDisplay {
-    /**
-     * Invoked when the find status changes.
-     */
-    public void setStatus(String text, boolean isError);
-}
