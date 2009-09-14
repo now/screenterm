eb834566ec2dce9d37477a76d44263c84eb40fe9
Nikolai Weibull <now@bitwi.se>
2009-09-14 18:23:00 +0200

    Remove final unadvertized single-character escapes

--- src/terminator/terminal/escape/SingleCharEscapeAction.java
+++ src/terminator/terminal/escape/SingleCharEscapeAction.java
@@ -22,23 +22,6 @@ public class SingleCharEscapeAction implements TerminalAction {
 			case 'M':  // Move cursor up one line, scrolling if it reaches the top of scroll region.  Opposite of NL.
 				model.scrollDisplayUp();
 				break;
-				
-			// Change character set.
-			// Note that these are different to the related ^N and ^O sequences, which select character sets 1 and 0 and are handled elsewhere.
-			// These sequences ("^[n" and "^[o") are even less common than their relatives.
-			case 'n':
-				control.invokeCharacterSet(2);
-				break;
-			case 'o':
-				control.invokeCharacterSet(3);
-				break;
-				
-			case '|':
-			case '}':
-			case '~':
-				// Invoke the G3, G2, and G1 character sets as
-				// GR. Has no visible effect.
-				break;
 			default:
 				Log.warn("Unrecognized single-character escape \"" + escChar + "\".");
 		}
@@ -46,25 +29,12 @@ public class SingleCharEscapeAction implements TerminalAction {
 	
 	private String getType() {
 		switch (escChar) {
-		case 'D': return "Down one line";
-		case 'E': return "Move to start of next line";
-		case 'H': return "Set tab at cursor";
 		case 'M': return "Cursor up one line";
-		case 'Z': return "Send device attributes (obsolete)";
-		case 'n': return "Invoke character set 2";
-		case 'o': return "Invoke character set 3";
-		case '|':
-		case '}':
-		case '~': return "Invoke G3, G2, G1 character sets as GR";
 		default: return "Unrecognized:" + escChar;
 		}
 	}
 	
 	public String toString() {
 		return "SingleCharEscapeAction[" + getType() + "]";
-	}
-	
-	private void unsupported(String description) {
-		Log.warn("Unsupported single-character escape \"" + escChar + "\" (" + description + ").");
 	}
 }
