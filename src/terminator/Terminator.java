33ba1b9afc35566956bb8e46e07642cec12c5a9e
Nikolai Weibull <now@bitwi.se>
2009-09-29 23:10:25 +0200

    Actually remove the bold color stuff
    Damnit.  I really need to compile before I commit.

--- src/terminator/Terminator.java
+++ src/terminator/Terminator.java
@@ -107,8 +107,6 @@ public class Terminator {
 	}
 	
 	private void optionsDidChange() {
-		boldForegroundColor = Palettes.getBrightColorFor(new Color(0x181818));
-		
 		// On the Mac, the Command key (called 'meta' by Java) is always used for keyboard equivalents.
 		// On other systems, Control tends to be used, but in the special case of terminal emulators this conflicts with the ability to type control characters.
 		// The traditional work-around has always been to use Alt, which -- conveniently for Mac users -- is in the same place on a PC keyboard as Command on a Mac keyboard.
