5658205e0e20fa1b1d95a97c6869dc49ecf5a58f
Nikolai Weibull <now@bitwi.se>
2009-09-30 22:56:03 +0200

    Use own palette

--- src/terminator/Palettes.java
+++ src/terminator/Palettes.java
@@ -3,38 +3,26 @@ package terminator;
 import java.awt.Color;
 
 public class Palettes {
-        /** Tango palette from gnome-terminal 2.24. */
-        private static final Color[] TANGO_COLORS = new Color[] {
-                new Color(0x2e3436),
-                new Color(0xcc0000),
-                new Color(0x4e9a06),
-                new Color(0xc4a000),
-                new Color(0x3465a4),
-                new Color(0x75507b),
-                new Color(0x06989a),
-                new Color(0xd3d7cf),
-                new Color(0x555753),
-                new Color(0xef2929),
-                new Color(0x8ae234),
-                new Color(0xfce94f),
-                new Color(0x729fcf),
-                new Color(0xad7fa8),
-                new Color(0x34e2e2),
-                new Color(0xeeeeec),
+        private static final Color[] COLORS = new Color[] {
+                new Color(0x000000),
+                new Color(0x951616),
+                new Color(0x257325),
+                new Color(0x766020),
+                new Color(0x2f5a9b),
+                new Color(0x602f80),
+                new Color(0x5694a8),
+                new Color(0xc0c0c0),
+                new Color(0x181818),
+                new Color(0xf02626),
+                new Color(0x009000),
+                new Color(0xf0a500),
+                new Color(0x2080c0),
+                new Color(0x933763),
+                new Color(0x80b0c0),
+                new Color(0xf6f6f6),
         };
 
-        private static Color[] currentPalette() {
-                return TANGO_COLORS;
-        }
-
-        /**
-         * Returns the color corresponding to 'index' (0-7), in its normal or bright variant.
-         */
         public static Color getColor(int index, boolean bright) {
-                if (!bright) {
-                        return currentPalette()[index];
-                } else {
-                        return currentPalette()[index + 8];
-                }
+                return COLORS[index + (bright ? 8 : 0)];
         }
 }
