5ad3e992eb2a96419ee1d2a557b644c37d3f5aa8
Nikolai Weibull <now@bitwi.se>
2009-10-27 21:21:40 +0100

    Remove frame resizing in TerminatorFrame
    We don’t need this anymore, as we don’t deal with tabs anymore.

--- src/terminator/TerminatorFrame.java
+++ src/terminator/TerminatorFrame.java
@@ -32,73 +32,15 @@ public class TerminatorFrame extends JFrame {
 				}
 			});
 		}
-		
-		initTerminal();
-                updateMenuBar();
-		
-		pack();
+
+                setContentPane(terminal);
+                setJMenuBar(new TerminatorMenuBar());
+                pack();
+
                 setLocationRelativeTo(null);
-		setVisible(true);
+                setVisible(true);
 	}
 	
-	private void initTerminal() {
-		Dimension initialSize = terminal.getSize();
-
-		terminal.invalidate();
-		setContentPane(terminal);
-		validate();
-
-		terminal.requestFocus();
-
-                /* TODO: I don’t think this will be needed any more. */
-		Dimension finalSize = getContentPane().getSize();
-		fixTerminalSizesAfterAddingOrRemovingTabbedPane(initialSize, finalSize);
-	}
-	
-	/**
-	 * It's okay to call this multiple times, and we deliberately do so whenever the preferences change.
-	 * This lets us update suggested keystrokes when the use-alt-as-meta option changes.
-	 */
-	private void updateMenuBar() {
-		// Replace any existing menu bar.
-		setJMenuBar(new TerminatorMenuBar());
-		
-		// Work around Sun bug 4949810 (setJMenuBar doesn't call revalidate/repaint).
-		getJMenuBar().revalidate();
-	}
-	
-	/**
-	 * Increases the size of the frame based on the amount of space taken
-	 * away from the terminal to insert the tabbed pane. The end result
-	 * should be that the terminal size remains constant but the window
-	 * grows.
-	 */
-	private void fixTerminalSizesAfterAddingOrRemovingTabbedPane(Dimension initialSize, Dimension finalSize) {
-		// GNOME's current default window manager automatically ignores setSize if the window is maximized.
-		// Windows doesn't, and that causes us to resize a maximized window to be larger than the display, which is obviously unwanted.
-		// This early exit fixes Windows' behavior and doesn't hurt Linux.
-		if ((getExtendedState() & MAXIMIZED_BOTH) == MAXIMIZED_BOTH) {
-			return;
-		}
-		
-		Dimension size = getSize();
-		size.height += (initialSize.height - finalSize.height);
-		size.width += (initialSize.width - finalSize.width);
-		
-		// We dealt above with the case where the window is maximized, but we also have to deal with the case where the window is simply very tall.
-		// GNOME and Mac OS will correctly constrain the window for us, but on Windows we have to try to do it ourselves.
-		if (GuiUtilities.isWindows()) {
-			Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-			Insets screenInsets = Toolkit.getDefaultToolkit().getScreenInsets(getGraphicsConfiguration());
-			final int availableVerticalScreenSpace = screenSize.height - screenInsets.top - screenInsets.bottom;
-			if (getLocation().y + size.height > availableVerticalScreenSpace) {
-				size.height = availableVerticalScreenSpace - getLocation().y;
-			}
-		}
-		
-		setSize(size);
-	}
-
         public boolean isShowingOnScreen() {
                 return isShowing() && (getExtendedState() & ICONIFIED) == 0;
         }
