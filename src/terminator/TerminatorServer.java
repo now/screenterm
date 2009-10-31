b2e49f3fad47cf533e4da288360fe6ccfad7988a
Nikolai Weibull <now@bitwi.se>
2009-10-31 13:59:35 +0100

    Clean up TerminatorServer
    It should be noted that TerminatorServer allows arbitrary code
    execution.  This is only OK because we trust the file security on the
    secret file.  Furthermore, we only accept connections from localhost.

--- src/terminator/TerminatorServer.java
+++ src/terminator/TerminatorServer.java
@@ -4,34 +4,18 @@ import e.util.*;
 import java.io.*;
 import java.util.*;
 
-/**
- * Improves the performance of opening a new shell from the command line or
- * from applications such as the GNOME panel.
- * We allow the socket to tell us to execute arbitrary code.
- * This is only OK because we trust the file security on the secret file.
- * We don't accept connections except from localhost.
- */
 public class TerminatorServer {
-    public void parseCommandLine(PrintWriter out, String line) {
-        List<String> arguments = new ArrayList<String>();
-        String[] encodedArguments = line.split(" ");
-        for (String encodedArgument : encodedArguments) {
-            String argument = StringUtilities.urlDecode(encodedArgument);
-            arguments.add(argument);
+        public void parseCommandLine(PrintWriter out, String line) {
+                List<String> arguments = new ArrayList<String>();
+                for (String encoded : line.split(" "))
+                        arguments.add(StringUtilities.urlDecode(encoded));
+                arguments.remove(0);
+                TerminatorOpener opener = new TerminatorOpener(arguments, out);
+                if (opener.showUsageIfRequested(out))
+                        return;
+                TerminatorFrame window = opener.openFromBackgroundThread();
+                if (window == null)
+                        return;
+                GuiUtilities.waitForWindowToDisappear(window);
         }
-        // Discard this method's name.
-        arguments.remove(0);
-        // We only have one pipe.
-        PrintWriter err = out;
-        TerminatorOpener opener = new TerminatorOpener(arguments, err);
-        if (opener.showUsageIfRequested(out)) {
-            return;
-        }
-        TerminatorFrame window = opener.openFromBackgroundThread();
-        if (window == null) {
-            // Any syntax error will have been reported.
-            return;
-        }
-        GuiUtilities.waitForWindowToDisappear(window);
-    }
 }
